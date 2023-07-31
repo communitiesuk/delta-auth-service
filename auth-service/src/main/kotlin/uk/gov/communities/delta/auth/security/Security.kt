715a683939d94cf602a442232584a969265c5581
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2023-07-31 12:00:31 +0100

    DT-335 Add logging for client header bearer auth

--- auth-service/src/main/kotlin/uk/gov/communities/delta/auth/security/Security.kt
+++ auth-service/src/main/kotlin/uk/gov/communities/delta/auth/security/Security.kt
@@ -2,7 +2,9 @@ package uk.gov.communities.delta.auth.security
 
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import org.slf4j.LoggerFactory
 import uk.gov.communities.delta.auth.Injection
+import uk.gov.communities.delta.auth.config.OAuthClient
 
 // "Delta-Client: client-id:secret" header auth for internal APIs
 const val CLIENT_HEADER_AUTH_NAME = "delta-client-header-auth"
@@ -11,11 +13,13 @@ const val CLIENT_HEADER_AUTH_NAME = "delta-client-header-auth"
 const val DELTA_AD_LDAP_SERVICE_USERS_AUTH_NAME = "delta-ldap-service-users-basic"
 
 // Bearer token with an access token issued by this service's /auth-internal/token endpoint for internal APIs
+// Requires client header auth
 const val OAUTH_ACCESS_BEARER_TOKEN_AUTH_NAME = "oauth-bearer-access-token"
 
 fun Application.configureSecurity(injection: Injection) {
     val ldapAuthenticationService = injection.ldapServiceUserAuthenticationService()
     val oAuthSessionService = injection.oAuthSessionService
+    val logger = LoggerFactory.getLogger("Application.Security")
 
     authentication {
         basic(DELTA_AD_LDAP_SERVICE_USERS_AUTH_NAME) {
@@ -33,8 +37,12 @@ fun Application.configureSecurity(injection: Injection) {
         bearer(OAUTH_ACCESS_BEARER_TOKEN_AUTH_NAME) {
             realm = "auth-service"
             authenticate {
-                val clientPrincipal = principal<ClientPrincipal>(CLIENT_HEADER_AUTH_NAME) ?: return@authenticate null
-                oAuthSessionService.retrieveFomAuthToken(it.token, clientPrincipal.client)
+                val clientPrincipal = principal<ClientPrincipal>(CLIENT_HEADER_AUTH_NAME)
+                if (clientPrincipal == null) {
+                    logger.warn("OAuth Bearer token authentication, rejecting due to missing client authentication")
+                    return@authenticate null
+                }
+                oAuthSessionService.retrieveFomAuthToken(it.token, clientPrincipal.client as OAuthClient)
             }
         }
     }
