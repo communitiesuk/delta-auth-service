f4b9ff491d1225441870fefcda4e8b53064fe789
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2024-04-19 16:17:08 +0100

    DT-696 Update email content and subject

--- auth-service/src/main/kotlin/uk/gov/communities/delta/auth/services/EmailService.kt
+++ auth-service/src/main/kotlin/uk/gov/communities/delta/auth/services/EmailService.kt
@@ -247,7 +247,7 @@ class EmailService(
         sendTemplateEmail(
             "user-added-to-dclg-in-access-group",
             EmailContacts(recipients, emailConfig),
-            "DLUHC Delta: User has been added to collection '$accessGroupDisplayName'",
+            "DLUHC Delta: User has been added to collection group '$accessGroupDisplayName'",
             mapOf(
                 "deltaUrl" to deltaConfig.deltaWebsiteUrl,
                 "userFullName" to userName,
