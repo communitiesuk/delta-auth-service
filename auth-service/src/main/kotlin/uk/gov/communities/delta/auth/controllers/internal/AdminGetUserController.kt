d56760662f4c84f3de24cad660e6ff639a177063
PhoebeJackson1 <phoebe.jackson@softwire.com>
2024-05-17 15:38:38 +0100

    DT-976: Improve get user error handling

--- auth-service/src/main/kotlin/uk/gov/communities/delta/auth/controllers/internal/AdminGetUserController.kt
+++ auth-service/src/main/kotlin/uk/gov/communities/delta/auth/controllers/internal/AdminGetUserController.kt
@@ -1,13 +1,15 @@
 package uk.gov.communities.delta.auth.controllers.internal
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.slf4j.LoggerFactory
+import uk.gov.communities.delta.auth.plugins.ApiError
 import uk.gov.communities.delta.auth.services.DeltaSystemRole
+import uk.gov.communities.delta.auth.services.LdapUserWithRoles
 import uk.gov.communities.delta.auth.services.UserLookupService
 import uk.gov.communities.delta.auth.utils.getUserFromCallParameters
-import uk.gov.communities.delta.auth.utils.getUserGUIDFromCallParameters
 
 class AdminGetUserController(
     private val userLookupService: UserLookupService,
@@ -24,21 +26,19 @@ class AdminGetUserController(
             arrayOf(DeltaSystemRole.ADMIN, DeltaSystemRole.READ_ONLY_ADMIN), call
         )
 
-        // TODO DT-1022 - make this get userGUID instead
-        val user = getUserFromCallParameters(
-            call.request.queryParameters,
-            userLookupService,
-            "An error occurred when getting user details",
-            "get_user"
-        )
-        logger.atInfo().log("Getting info for user ${user.getUUID()}")
-//        val userWithRoles: LdapUserWithRoles
-//        try { // TODO DT-1022 - add the try catch back in and change back to lookupUserByGUIDAndLoadRoles
-        val userWithRoles = userLookupService.loadUserRoles(user)
-//        } catch (e: LdapRepository.NoUserException) {
-//            logger.warn("No user found with GUID $userGUID")
-//            return call.respond(HttpStatusCode.NotFound, "User not found")
-//        }
+        val userWithRoles: LdapUserWithRoles
+        try {
+            val user = getUserFromCallParameters( // TODO DT-1022 - make this get userGUID instead
+                call.request.queryParameters,
+                userLookupService,
+                "An error occurred when getting user details",
+                "get_user"
+            )
+            userWithRoles = userLookupService.loadUserRoles(user) // TODO DT-1022 - change back to lookupUserByGUIDAndLoadRoles
+        } catch (e: ApiError) {
+            logger.warn(e.errorDescription)
+            return call.respond(HttpStatusCode.NotFound, "User not found")
+        }
         call.respond(userWithRoles)
     }
 }
