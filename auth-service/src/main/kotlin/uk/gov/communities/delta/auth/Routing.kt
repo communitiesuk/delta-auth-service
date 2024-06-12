c7c47bb52ad7d833302d2ef1851f7a76cba3020b
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2024-06-12 18:26:54 +0100

    DT-836 remove nonexistent CORS imports

--- auth-service/src/main/kotlin/uk/gov/communities/delta/auth/Routing.kt
+++ auth-service/src/main/kotlin/uk/gov/communities/delta/auth/Routing.kt
@@ -4,7 +4,6 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.http.content.*
-import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.ratelimit.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
