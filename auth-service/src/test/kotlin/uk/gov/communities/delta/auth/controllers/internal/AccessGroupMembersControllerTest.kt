ca90681b0797e6a44e68f49f7d95b221800fb552
Yousuf Desai <119860263+YD-DLUHC@users.noreply.github.com>
2024-10-10 15:32:23 +0100

    Fix for DT-1161: adds org id to user to enable filtering in frontend (#174)
    * Fix for DT-1161: adds org id to user to enable filtering in frontend

--- auth-service/src/test/kotlin/uk/gov/communities/delta/auth/controllers/internal/AccessGroupMembersControllerTest.kt
+++ auth-service/src/test/kotlin/uk/gov/communities/delta/auth/controllers/internal/AccessGroupMembersControllerTest.kt
@@ -91,6 +91,7 @@ class AccessGroupMembersControllerTest {
                 objectGUID = "guid1",
                 mail = "user1@example.com",
                 fullName = "User One",
+                organisationID = "orgCode1",
                 roles = listOf("Data provider")
             ),
             LdapRepository.UserWithRoles(
@@ -98,6 +99,7 @@ class AccessGroupMembersControllerTest {
                 objectGUID = "guid2",
                 mail = "user2@example.com",
                 fullName = "User Two",
+                organisationID = "orgCode2",
                 roles = listOf("Data certifier")
             )
         )
