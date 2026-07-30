2ad7e6a9ce3173ef81785135e3bdbbbb922b308b
Keith Kennedy <keith.kennedy@communities.gov.uk>
2026-07-30 08:27:19 +0100

    DT-2948: al2023 bump (#292)
    * DT-2948_al2023_bump
    * Bump publish version

--- terraform/modules/auth_service/main.tf
+++ terraform/modules/auth_service/main.tf
@@ -80,7 +80,7 @@ module "fargate" {
     },
     {
       name  = "DATABASE_URL"
-      value = "jdbc:postgresql://${aws_db_instance.auth_service.endpoint}/auth_service?ssl=true&sslmode=verify-full&sslrootcert=/root/.postgresql/ca-bundle.pem"
+      value = "jdbc:postgresql://${aws_db_instance.auth_service.endpoint}/auth_service?ssl=true&sslmode=verify-full&sslrootcert=/app/certs/rds-ca-bundle.pem"
     },
     {
       name  = "DATABASE_USER"
